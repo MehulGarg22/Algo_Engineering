01fc61a6928b7078a2d91dd7712638710c996dcf
Mehul Garg <mehul.garg104@gmail.com>
2021-04-28 12:33:51 +0530

    Add files via upload

---
+++ Program/Stack/Stack_Reverse_String.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void reversestring(string s)
+{
+    stack<string>st;
+    for(int i=0;i<s.length();i++)
+    {
+        string word=" ";
+        while(s[i]!=' ' && i<s.length())
+        {
+            word+=s[i];
+            i++;
+        }
+        st.push(word);
+    }
+
+    while(!st.empty()){
+        cout<<st.top();
+        st.pop();
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    string s="Hey, Boy I am Mehul";
+    reversestring(s);
+return 0;
+}
