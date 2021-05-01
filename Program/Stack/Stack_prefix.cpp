37b54780a87b00d5376c15c826e2876a59b30d8a
Mehul Garg <mehul.garg104@gmail.com>
2021-05-01 23:44:27 +0530

    Add files via upload

---
+++ Program/Stack/Stack_prefix.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<stack>
+using namespace std;
+
+int prefix(string s)
+{
+    int i=0;
+    stack <int> st;
+    for(i=s.length()-1; i>=0; i--)
+    {
+        if(s[i]>='0' && s[i]<='9'){
+            st.push(s[i]-'0');
+        }
+        else{
+            int op1=st.top();
+            st.pop();
+            int op2=st.top();
+            st.pop();
+            switch(s[i]){
+                case '+':
+                    st.push(op1+op2);
+                    break;
+                case '-':
+                    st.push(op1-op2);
+                    break;
+                case '*':
+                    st.push(op1*op2);
+                    break;
+                case '/':
+                    st.push(op1/op2);
+                    break;
+                case '^':
+                    st.push(pow(op1,op2));
+                    break;
+            }
+
+        }
+    }
+    return st.top();
+}
+
+int main()
+{
+    cout<<prefix("-+7*45+20");
+    return 0;
+}
