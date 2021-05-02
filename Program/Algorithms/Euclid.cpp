8f6f40fa010b2f2afc5cc36fbe546d6fd2d7ae96
Mehul Garg <mehul.garg104@gmail.com>
2021-05-03 00:04:01 +0530

    Create Euclid.cpp

---
+++ Program/Algorithms/Euclid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int GCD(int a, int b)
+{
+    while(b!=0)
+    {
+        int rem=a%b;
+        a=b;
+        b=rem;
+    }
+
+    return b;
+}
+int main()
+{
+    int a, b;
+    cout<<"Enter Number: ";
+    cin>>a>>b;
+
+    cout<<GCD(a, b);
+return 0;
+}
