b1b8b2f86804c41ff871ed3cf6513a5b4cd89c45
Mehul Garg <mehul.garg104@gmail.com>
2021-05-03 00:12:24 +0530

    Create Exponential.cpp

---
+++ Program/Recursion/Exponential.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+
+
+int main(){
+int n, p;
+cout<<"Enter the Number and Power: ";
+cin>>n>>p;
+cout<<pow(n, p);
+return 0;
+}
