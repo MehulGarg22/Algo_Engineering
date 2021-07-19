75290b59973ac11e7124ea78acfb6992e66d23a9
unknown <mehul.garg104@gmail.com>
2021-07-19 15:29:13 +0530

    Print All Node of BST  in a Vertical Order

---
+++ Program/Hashing/Hashing_VerticalOrder.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <map>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    Node *left;
+    Node *right;
+    int data;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void VerticalOrder(Node *root, int hdis, map<int, vector<int>> &mp)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    mp[hdis].push_back(root->data);
+    VerticalOrder(root->left, hdis - 1, mp);
+    VerticalOrder(root->right, hdis + 1, mp);
+}
+
+void Printorder(map<int, vector<int>> mp)
+{
+    map<int, vector<int>>::iterator it;
+    for (it = mp.begin(); it != mp.end(); it++)
+    {
+        for (int i = 0; i < (it->second).size(); i++)
+        {
+            cout << (it->second)[i] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    root->left = new Node(7);
+    root->right = new Node(4);
+    root->left->left = new Node(3);
+    root->left->right = new Node(11);
+    root->right->left = new Node(14);
+    root->right->right = new Node(6);
+
+    map<int, vector<int>> mp;
+    int hdis = 0;
+    VerticalOrder(root, hdis, mp);
+    Printorder(mp);
+
+    return 0;
+}
