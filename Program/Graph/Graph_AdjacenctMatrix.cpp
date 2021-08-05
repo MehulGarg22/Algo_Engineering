10918e72e1a4b43ecd50703363d0a6a1362c7a51
unknown <mehul.garg104@gmail.com>
2021-08-05 14:52:21 +0530

    Adjanceny Matrix Graph Algo

---
+++ Program/Graph/Graph_AdjacenctMatrix.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+void print (vector<vector<int>> adm, int node)
+{
+    for(int i=1; i<node+1; i++){
+        for(int j=1; j<node+1; j++){
+            cout<<adm[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    int node, edge;
+    cout<<"Enter Number of Nodes, edges: ";
+    cin>>node>>edge;
+    vector<vector<int>> adm(node+1, vector<int>(node+1, 0));
+    for(int i=0; i<edge; i++){
+        int x, y;
+        cin>>x>>y;
+        adm[x][y]=1;
+        adm[y][x]=1;
+    }
+    cout<<endl;
+    cout<<"Adjacency Matrix formed is: "<<endl;
+    print(adm, node);
+
+return 0;
+}
